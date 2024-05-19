ebe2a4fa13536f802ac66cdcefea74281d18fddb
kello22e <kello22e@mtholyoke.edu>
2024-05-19 16:18:52 -0400

    changed parameters for each experiment

--- R/parameters.R
+++ R/parameters.R
@@ -1,4 +1,5 @@
 #path to functions.R file
+raw_data = "/Volumes/Seagate Portable Drive/Emily/20240516_fluovolt_fluo8_senktide_nkb_SP/Rao_lab_voltage_fluidics_20240516114631/scan/Well__D_002/FluoVolt/"
 functions = "/Users/emilykellogg/Documents/GitHub/intensity_analysis/R/startup/functions.R"
 
 #paths to fiji macros used to measure ROIs
